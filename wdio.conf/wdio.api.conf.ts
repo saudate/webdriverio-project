e4d2eca2e771c5db29005266bd110b0fa5d0fb85
extrah8 <m.tcrkv@gmail.com>
2025-10-12 22:37:18 +0300

    feat: update API test workflow and configuration for improved reporting and consistency

--- wdio.conf/wdio.api.conf.ts
+++ wdio.conf/wdio.api.conf.ts
@@ -1,30 +1,44 @@
 import { config as baseConfig } from './wdio.base.conf';
 
-export const config = {
+export const config: WebdriverIO.Config = {
   ...baseConfig,
 
   runner: 'local',
 
   specs: ['../test/specs/api/**/*.ts'],
 
+  services: [],
+
+  maxInstances: 1,
+
   capabilities: [
     {
       browserName: 'chrome',
       'goog:chromeOptions': {
-        args: ['--headless', '--disable-gpu'],
+        args: [
+          '--headless=new',
+          '--no-sandbox',
+          '--disable-dev-shm-usage',
+          '--disable-gpu'
+        ],
       },
+      pageLoadStrategy: 'eager',
     },
   ],
 
-  services: [],
-
   framework: 'mocha',
-  maxInstances: 1,
-  reporters: ['spec'],
+
+  reporters: [
+    'spec',
+    ['junit', {
+      outputDir: 'junit-report',
+      outputFileFormat: (opts: any) => `api-${opts.cid}.xml`
+    }]
+  ],
 
   mochaOpts: {
     ui: 'bdd',
-    timeout: 60000,
+    timeout: 60_000,
   },
 
   baseUrl: 'https://demoqa.com/',
